3ae7a544b4b352862af591a1da01a80e609600f1
esteb <esteb@Eazuara>
2017-03-03 20:35:11 -0600

    Branch

---
+++ src/bankbranches/Customer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankbranches;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author esteb
+ */
+public class Customer {
+
+    private ArrayList<Double> transactions = new ArrayList();
+    private String name;
+    
+    public Customer(String name, double initialAmount){
+        this.name=name;
+        this.transactions.add(initialAmount);
+    }
+    public void addTransaction(double amount){
+        this.transactions.add(amount);
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+    public String getName() {
+        return name;
+    }
+    
+}
